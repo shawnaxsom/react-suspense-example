2a736cbb161d02f833c832636d1c59e412eec003
Shawn Axsom <axs221@gmail.com>
2019-08-16 00:06:36 -0400

    React Suspense is working. Styling isn't great.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Suspense } from "react";
+import "./App.scss";
+import "./checkmarks.scss";
+import { withCache } from "./components/withCache";
+import { createResource } from "simple-cache-provider";
 
-const App: React.FC = () => {
+const sleep = ms => new Promise(r => setTimeout(() => r(), ms));
+
+const readShows = createResource(async function fetchNews() {
+  await sleep(3000);
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  return await res.json();
+});
+
+const Todo = ({ item }) => {
+  const [checked, setChecked] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="column is-4 todo">
+      <section title=".squaredTwo">
+        <div className="squaredTwo">
+          <input
+            id="styled-checkbox-2"
+            type="checkbox"
+            value="value2"
+            checked={checked}
+            onClick={() => setChecked(!checked)}
+          />
+          <label htmlFor={`styled-checkbox-2 ${checked ? "checked" : ""}`}>
+            {item.title}
+          </label>
+        </div>
+      </section>
     </div>
   );
-}
+};
 
-export default App;
+const Todos = withCache(props => {
+  const result = readShows.read(props.cache);
+
+  if (!result) {
+    return null;
+  }
+
+  return (
+    <React.Fragment>
+      {result.map(item => (
+        <Todo key={item.title} item={item} />
+      ))}
+    </React.Fragment>
+  );
+});
+
+const TodoContainer = () => {
+  const children = <Todos />;
+  const fallback = <progress className="progress is-info" max="100" />;
+
+  return (
+    <div className="container">
+      <div className="columns is-multiline">
+        <Suspense fallback={fallback}>{children}</Suspense>
+      </div>
+    </div>
+  );
+};
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Todo List</h1>
+          </header>
+
+          <TodoContainer />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
